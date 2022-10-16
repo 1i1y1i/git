3cd6406a5bc6a3b44f4b2322cd0abc4cdf668fad
lxn <2819773368@qq.com>
2022-10-16 09:33:30 +0800

    Initial commit

---
+++ src/main/java/com/powernodel/git/TestGit.java
@@ -0,0 +1,10 @@
+package com.powernodel.git;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("sout");
+        System.out.println("lalall");
+        System.out.println("hhhhhhh");
+        System.out.println("llllll");
+    }
+}
